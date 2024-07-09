4e8c1e024b0122f851ecfaf54df78d352d8d9b20
Vedant Parkhe <vedantparkhe2409@gmail.com>
2024-07-10 00:19:33 +0530

    Logs added

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,13 +1,12 @@
 import { connectToDb } from "@utils/database";
 import Prompt from "@models/Prompt";
-import User from "@models/User";
 
 export const GET = async (req) => {
     try {
         await connectToDb();
-
+        console.log("Database connected successfully");
         const prompts = await Prompt.find({}).populate('creator');
-
+        console.log("Fetched prompts:", prompts);
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
         console.log(error)
