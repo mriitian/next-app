d0f9090845beab2d19306a4e6c9a1fde57639a50
Vedant Parkhe <vedantparkhe2409@gmail.com>
2024-07-10 00:31:03 +0530

    Dynamic added

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,6 +1,6 @@
 import { connectToDb } from "@utils/database";
 import Prompt from "@models/Prompt";
-
+export const dynamic = 'force-dynamic';
 export const GET = async (req) => {
     try {
         await connectToDb();
